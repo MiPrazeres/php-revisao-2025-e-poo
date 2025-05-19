c6df3b3047b5f13023f04689202f3ede61489e0f
MiPrazeres <milena.prazeres@aluno.senai.br>
2025-05-19 08:29:15 -0300

    revisão de PHP
    realizada

--- 2_POO vs Estruturada_alunos/2_comparacao-php_alunos/estruturada.php
+++ 2_POO vs Estruturada_alunos/2_comparacao-php_alunos/estruturada.php
@@ -1,10 +1,56 @@
 <?php
 // ==========================================
-// PARTE 1: PROGRAMAÇÃO ESTRUTURADA
+// PARTE 2: PROGRAMAÇÃO ORIENTADA A OBJETOS
 // ==========================================
 
+class cachorro {
+    private $nome;
+    private $comida;
+    private $sono;
 
-// 1ª Digitação (Aqui)
+    public function __construct($nome, $comida, $sono) {
+        $this->nome = $nome;
+        $this->comida = $comida;
+        $this->sono = $sono;
+    }
 
+    public function comer() {
+        $this->comida -= 1;
+    }
 
+    public function dormir() {
+        $this->sono = false;
+    }
+
+    // GETters e SETtes comuns no php
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getComida() {
+        return $this->comida;
+    }
+
+    public function getSono() {
+        return $this->sono;
+    }
+}
+
+$cachorro1 = new cachorro("Nelson", 3, false);
+$cachorro2 = new cachorro("Jeremias", 1, true);
+
+// Exibindo os resultados no navegador
+echo "<!DOCTYPE html>
+<html lang='pt-BR'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Resultados dos Cachorros (POO)</title>
+</head>
+<body>
+    <h1>Resultados dos Cachorros (POO)</h1>
+    <p><strong>{$cachorro_1->getNome()}</strong> agora tem <strong>{$cachorro_1->getComida()}</strong> unidades de comida.</p>
+    <p><strong>{$cachorro_2->getNome()}</strong> está com sono? <strong>" . ($cachorro_2->getSono() ? 'Sim' : 'Não') . "</strong></p>
+</body>
+</html>";
 ?>
